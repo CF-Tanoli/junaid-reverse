7095fb408e0d51df0e98da63c9ed307cc50d6ec6
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-10-10 18:35:27 +0500

    Create aahil-reverse.py

---
+++ aahil-reverse.py
@@ -0,0 +1,14 @@
+import os, platform
+os.system("clear")
+print(" or jb new version upload kro fr chalnge kr lena ")
+time.sleep(3)
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Lite.py
